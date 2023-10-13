8017fc38135b7fb6dfd83c0104a9b22da242bf6b
Stefan Marr <git@stefan-marr.de>
2023-10-13 11:56:58 +0100

    Added initial demo to check consistency of list contains and size

---
+++ 010_conc_insert.py
@@ -0,0 +1,28 @@
+from threading import Thread
+
+
+def insert_remove_fn(list):
+    for _ in range(100_000_000):
+        list.append(1)
+        list.pop()
+
+
+def contains_and_size_fn(list):
+    for _ in range(100_000_000):
+        l = len(list)
+        c = 1 in list
+        assert (not c and l == 0) or (c and l == 1)
+
+
+shared_list = []
+threads = [
+    Thread(target=insert_remove_fn, args=(shared_list,)),
+    Thread(target=contains_and_size_fn, args=(shared_list,)),
+]
+
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+
+print("Done")
