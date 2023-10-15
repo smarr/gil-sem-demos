7920ccff799193ae31430ab8f934d3d70b53ecfc
Stefan Marr <git@stefan-marr.de>
2023-10-15 15:20:19 +0100

    Added a demo to show the assertion tripping with a GIL

---
+++ 012_conc_insert.py
@@ -0,0 +1,34 @@
+from gil_sem import ResultThread as Thread, start_and_await
+import sys
+import time
+
+sys.setswitchinterval(0.0000001)
+
+
+def insert_remove_fn(list):
+    for i in range(100_000_000):
+        list.append(1)
+        time.sleep(0.0)
+        list.pop()
+    return True
+
+
+def contains_and_size_fn(list):
+    for _ in range(100_000_000):
+        l = len(list)
+        time.sleep(0.0)
+        c = 1 in list
+        assert (not c and l == 0) or (c and l == 1)
+    return True
+
+
+shared_list = []
+threads = [
+    Thread(insert_remove_fn, (shared_list,)),
+    Thread(contains_and_size_fn, (shared_list,)),
+]
+
+results = start_and_await(threads)
+for r in results:
+    if r.assertion:
+        raise r.assertion
