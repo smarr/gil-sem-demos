a2ed2fa206dd6be9eb79c0f19980f59b6bb1d3f6
Stefan Marr <git@stefan-marr.de>
2023-11-13 21:36:40 +0000

    Add demo for the differences in GIL granularity in a numerical way

---
+++ 040_conc_insert_pairs.py
@@ -0,0 +1,45 @@
+from gil_sem import ResultThread as Thread, start_and_await, report_error_or_success
+import sys
+import time
+
+# This is to demonstrate the granularity of the GIL.
+# This test is expected to "fail" and report how many tries it took before
+# seeing non-atomic loop bodies. The reported number depends on the used
+# Python implementation and version.
+
+
+def insert_fn(list):
+    for i in range(100_000_000):
+        list.append(1)
+        list.append(1)
+        list.pop()
+        list.pop()
+    return True
+
+
+def remove_fn(list):
+    for i in range(100_000_000):
+        try:
+            list.pop()
+            list.pop()
+        except IndexError:
+            pass
+    return True
+
+
+def size_fn(list):
+    for i in range(100_000_000):
+        l = len(list)
+        assert l % 2 == 0, f"List length was {l} at attempt {i}"
+    return True
+
+
+shared_list = []
+threads = [
+    Thread(insert_fn, (shared_list,)),
+    # Thread(remove_fn, (shared_list,)),
+    Thread(size_fn, (shared_list,)),
+]
+
+results = start_and_await(threads)
+report_error_or_success(results)
---
+++ 041_conc_insert_pairs.py
@@ -0,0 +1,40 @@
+from gil_sem import ResultThread as Thread, start_and_await, report_error_or_success
+
+# This is to demonstrate the granularity of the GIL.
+# This test is expected to "fail" and report how many tries it took before
+# seeing non-atomic loop bodies. The reported number depends on the used
+# Python implementation and version.
+
+
+def append(l):
+    l.append(1)
+    l.append(1)
+
+
+def remove(l):
+    l.pop()
+    l.pop()
+
+
+def insert_fn(list):
+    for i in range(100_000_000):
+        append(list)
+        remove(list)
+    return True
+
+
+def size_fn(list):
+    for i in range(100_000_000):
+        l = len(list)
+        assert l % 2 == 0, f"List length was {l}, at attempt {i}"
+    return True
+
+
+shared_list = []
+threads = [
+    Thread(insert_fn, (shared_list,)),
+    Thread(size_fn, (shared_list,)),
+]
+
+results = start_and_await(threads)
+report_error_or_success(results)
